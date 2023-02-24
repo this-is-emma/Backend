c8e3edfd5d603bc7abb396b0ea3ec2070f7f88d0
ThisIsEmma <sakatia.lise@gmail.com>
2023-02-23 22:34:17 -0500

    pretty printed deploy demo

--- foundation_app/config.py
+++ foundation_app/config.py
@@ -7,6 +7,7 @@ load_dotenv()
 class Config(object):
     """Set environment variables."""
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#postgres://db_yj2e_user:6Fk09FVsCnLrvOKz6rL7xf6rvDXnfQWN@dpg-cfs26jpgp3jqrlf3q4h0-a.ohio-postgres.render.com/db_yj2e
